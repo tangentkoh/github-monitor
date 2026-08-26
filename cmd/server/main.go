791f8cb067338f3f271f82aeeab6d8c5e9370dc8
unknown <168181534+tangentkoh@users.noreply.github.com>
2026-08-26 23:54:26 +0900

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -40,10 +40,10 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(200, "OK")
 	})
-	e.GET("/", func(c echo.Context) error {
+	e.Match([]string{"GET", "HEAD"}, "/", func(c echo.Context) error {
 		return c.String(200, "GitHub Monitor is Running!")
 	})
-	e.GET("/health", func(c echo.Context) error {
+	e.Match([]string{"GET", "HEAD"}, "/health", func(c echo.Context) error {
 		return c.String(200, "OK")
 	})
 
